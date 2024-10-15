781fbb98f44742f9da2c5cc677fded2e9d27a268
n7kss <97689303+musurico@users.noreply.github.com>
2024-10-15 17:55:50 +0300

    Add files via upload

---
+++ n6.jl
@@ -0,0 +1,29 @@
+using HorizonSideRobots
+
+robot=Robot(animate=true)
+
+function pole(robot)
+    d::Int = 0
+    l::Int = 0
+    left_ugol(robot)
+    perimetr(robot)
+end
+
+function left_ugol(robot)
+    while !isborder(robot, West) || !isborder(robot, Sud)
+        for side in (Sud, West)
+            while !isborder(robot, side)
+                move!(robot, side)
+            end
+        end
+    end
+end
+
+function perimetr(robot)
+    for side in (Ost, Nord, West, Sud)
+        while !isborder(robot, side)
+            putmarker!(robot)
+            move!(robot, side)
+        end
+    end
+end
